b8fb1d7be58123179152bfe277c5a819c4fea824
wali55 <walisantunu@gmail.com>
2023-08-07 18:44:00 +0600

    finish quiz app

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,14 +1,24 @@
 <script setup>
-    import { defineProps } from "vue";
+import { defineProps, defineEmits } from "vue";
 
-    const { question } = defineProps(["question"]);
+const emit = defineEmits(["selectOption"]);
+const { question } = defineProps(["question"]);
+
+const emitSelectedOption = (isCorrect) => {
+  emit("selectOption", isCorrect);
+};
 </script>
 
 <template>
   <div class="question-container">
     <h1 class="question">{{ question.text }}</h1>
   </div>
-  <div v-for="option in question.options" :key="option.id" class="options-container">
+  <div
+    v-for="option in question.options"
+    :key="option.id"
+    class="options-container"
+    @click="emitSelectedOption(option.isCorrect)"
+  >
     <p class="option-label">{{ option.label }}</p>
     <div class="option-value">
       <p>{{ option.text }}</p>
--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,28 +1,61 @@
 <script setup>
 import Question from "../components/Question.vue";
 import QuizHeader from "../components/QuizHeader.vue";
+import Results from "../components/Result.vue";
 import { useRoute } from "vue-router";
-import { ref, watch } from "vue";
+import { ref, computed } from "vue";
 import quizzes from "../data/quizzes.json";
 
 const route = useRoute();
 const quizId = parseInt(route.params.id);
 const quiz = quizzes.find((q) => q.id === quizId);
 const currentQuestionIndex = ref(0);
+const numberOfCorrectAnswers = ref(0);
+const showResults = ref(false);
 
-const questionStatus = ref(`${currentQuestionIndex.value} / ${quiz.questions.length}`);
+// const questionStatus = ref(`${currentQuestionIndex.value} / ${quiz.questions.length}`);
 
-watch(() => currentQuestionIndex.value, () => {
-    questionStatus.value = `${currentQuestionIndex.value} / ${quiz.questions.length}`;
-});
+// watch(() => currentQuestionIndex.value, () => {
+//     questionStatus.value = `${currentQuestionIndex.value} / ${quiz.questions.length}`;
+// });
+
+const questionStatus = computed(
+  () => `${currentQuestionIndex.value} / ${quiz.questions.length}`
+);
+const barPercentage = computed(
+  () => `${(currentQuestionIndex.value / quiz.questions.length) * 100}%`
+);
+
+const onOptionSelected = (isCorrect) => {
+  if (isCorrect) {
+    numberOfCorrectAnswers.value++;
+  }
+
+  if (quiz.questions.length - 1 === currentQuestionIndex.value) {
+    showResults.value = true;
+  }
+
+  currentQuestionIndex.value++;
+};
 </script>
 
 <template>
   <div>
-    <QuizHeader :questionStatus="questionStatus" />
+    <QuizHeader
+      :questionStatus="questionStatus"
+      :barPercentage="barPercentage"
+    />
     <div>
-      <Question :question="quiz.questions[currentQuestionIndex]" />
+      <Question
+        v-if="!showResults"
+        :question="quiz.questions[currentQuestionIndex]"
+        @selectOption="onOptionSelected"
+      />
+      <Results
+        v-else
+        :quizQuestionLength="quiz.questions.length"
+        :numberOfCorrectAnswers="numberOfCorrectAnswers"
+      />
     </div>
-    <button @click="currentQuestionIndex++">Next</button>
   </div>
 </template>
